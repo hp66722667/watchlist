5dd50930c38f082bbbc31cb8f6266c244e0e5b6b
hp66722667 <66722667@qq.com>
2019-03-21 15:07:58 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -43,9 +43,8 @@ class Movie(db.Model):  # 表名将会是 movie
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 
 @app.route('/user/<name>')
@@ -107,3 +106,14 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于return {'user': user}
